d13aa9b83fda09ac35ef1166b670af7a7185add1
byatham <balaiahy23@gmail.com>
2023-01-05 18:46:53 +0530

    added build spec.yml file

---
+++ src/main/java/com/anu/tech/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.anu.tech.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.anu.tech.util.MessageUtility;
+
+@RestController
+@RequestMapping("hello")
+public class HelloController {
+	
+	@GetMapping("/message")
+	public String getHello(@RequestParam String name)
+	{
+		return "Hello "+name+" "+MessageUtility.getWishMessage();
+	}
+
+}
